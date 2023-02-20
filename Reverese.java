d889856a5802fc0e93922657cb726b4d8d259266
sajalshrivastav <93852168+sajalshrivastav@users.noreply.github.com>
2023-02-20 22:52:01 +0530

    1st question reverse array

--- Reverese.java
+++ Reverese.java
@@ -1,5 +1,23 @@
 public class Reverese{
+      
+    static void revArr(int arr[], int start , int end){
+    
+    }
+    
+    
+    
+    static void printArr(int arr[] , int size){
+    
+    }
+    
+    
 public static void main(String[] args) {
+    int arr[]={1,2,3,4,5,6};
+    printArr=(arr,6);
+    revArr=(arr, 0 , 5);
+    System.out.println("The reverse array is :");
+    printArr=(arr,6);
+    
     
 }
 
