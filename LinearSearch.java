1ccb2da2561d8415d8415a3e39f4d8fbe4d944c4
sajalshrivastav <93852168+sajalshrivastav@users.noreply.github.com>
2023-03-13 21:36:19 +0530

    Create LinearSearch.java
    This is the linear search program in java

---
+++ LinearSearch.java
@@ -0,0 +1,20 @@
+public class Myclasas{
+  public static Linear(){
+  for(int i =0;i<arr.length;i++)
+  {
+        if(arr[i]==item){
+        System.out.println("Element found");
+          return;
+        }
+     System.out.println("Element not found");
+  }
+    
+  }
+  
+    public static void main(String[] args){
+      int arr[]={23,45,89,212,55};
+      int item=89;
+      Linear(arr, item);
+      
+    }
+}
